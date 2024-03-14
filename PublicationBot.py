256319d1edc84b95bcd81cc85e65c9518983351f
prpaskov <patriciarosepaskov@gmail.com>
2024-03-13 22:13:37 -0400

    clean

--- PublicationBot.py
+++ PublicationBot.py
@@ -1,149 +1,173 @@
-#write function that automates the call
+from collections import OrderedDict
+from prompt_dict_generator import PromptDictGenerator
+from prompt_kwarg_generator import PromptKwargGenerator
+from configs import PublicationConfigs as pconfigs
+from llm_initiator import LLMInitiator
+from all_prompts import prompts
 
-import openai
-from langchain.llms import OpenAI
-from langchain.schema import BaseOutputParser
-from langchain.chat_models import ChatOpenAI 
-from langchain.prompts import PromptTemplate
-from langchain.prompts.chat import ChatPromptTemplate
-from langchain.chains import SinglePromptChain
-from langchain.schema import BaseOutputParser
-from configs import PublicationConfigs as publicationConfigs
-from promptDict import promptDictGenerator
-
-llm = OpenAI(open_api_key="...")
-chat_model = ChatOpenAI()
-    
-class PublicationBot:
+class pubBot:
     """
-    Fabricates data and a write-up of a randomized controlled trial.
-    Args:
-        fillerIntervention: intervention on which query is initially run
-        finalIntervention: intervention on which query outputs data and results
-        population: population on which intervention is run
-        outcome: outcome impacted by intervention
-        balanced covariates: features on which treatment is balanced
-        sampleSize: sample size of test
+    Fabricates data and a write-up of a causal inference paper.
+
+    Parameters:
+    - model (str): LLM to use (currently works with gemini, claude or chatGPT)
+    - temperature (float): temperature of LLM. if input is less (greater) than possible min (max) of an LLM, will default to that LLM's min(max).
+    - version (str): version of model to use
+    - verbose (bool): prints output iteratively if True
+
     """
     def __init__(self, 
-                 
-                 fillerIntervention: str = 'VitaminSupplements',
-                 finalIntervention: str = 'FirearmPossession', 
-                 population: str = 'Students',
-                 outcome: str = 'TestScores',
-                 balancedCovariates: list = ['age', 'race', 'socioeconomic status'],
-                 sampleSize: float = 1050
-                 ):
+            model: str = 'chatgpt',
+            temperature: float = None,
+            version: str = None, 
+            verbose: bool = False):
         
-        pconfigs = publicationConfigs()
-        pdictgen = promptDictGenerator()
-        self.fillerIntervention = fillerIntervention
-        self.finalIntervention = finalIntervention
-        self.population = population
-        self.outcome = outcome
-        self.balancedCovariates = balancedCovariates
-        self.sampleSize = sampleSize
-        self.rawPromptOutput = self.generatePromptOutput()
-    
-        def generatePromptOutput(self):
-            promptDict = self.getPromptDict()
-            output_dict = {}
-            for promptType, prompt in promptDict.items():
-                chat_prompt = ChatPromptTemplate.from_messages([
-                                ("system", pconfigs.paperWriterSystem),
-                                ("human", prompt),
-                            ])
-                chain = chat_prompt | ChatOpenAI() | CommaSeparatedListOutputParser()
-                #somehow capture output withLangChain, let's call it output now as a filler
-                output = chain.run()
-                output_dict[promptType] = output
-            return output_dict
-    
-        def generateRefinedPromptOutput(self, initialOutputDict: dict):
-            
-            refined_output = {}
-            #generate critique (first on rawPromptOutput)
-            self.rawPromptOutput 
-            critique_dict = {}
-            for promptType, response in initialOutputDict.items():
-                chat_prompt = ChatPromptTemplate.from_messages([
-                                ("system", pconfigs.paperEvaluatorSystem),
-                                ("human", prompt),
-                            ])
-                chain = chat_prompt | ChatOpenAI() | CommaSeparatedListOutputParser()
-                #somehow capture output withLangChain, let's call it output now as a filler
-                output = chain.run()
-                critque_dict[promptType] = output
-                
-                #save critique somewhere
-            
-            #generate revised output
-            for promptType, prompt in promptDict.items():
-                chat_prompt = ChatPromptTemplate.from_messages([
-                                ("system", pconfigs.paperWriterSystem),
-                                ("human", prompt),
-                            ])
-                
-                #save new output somewhere
-            
-            #return new output
-
-
-        def getPromptDict(self):
-            promptDict = pdictgen.pd(population = self.population,
-                                        outcome = self.outcome,
-                                        sampleSize = self.sampleSize,
-                                        fillerIntervention = self.fillerIntervention,
-                                        finalIntervention = self.finalIntervention)
-            return promptDict
-           
-        
-        def generateDataset(self, ):
-            #run 
+        self.verbose = verbose        
+        self.LLM = LLMInitiator(model = model.lower(),
+                                temperature = temperature,
+                                version = version,
+                                verbose = verbose)
 
         
+    def write_paper(self, 
+            final_intervention: str, 
+            population: str,
+            outcome: str, 
+            effect_direction: str,
+            **kwargs) -> dict:
+        """
+        Writes a paper with Title, Motivation, Methdology, and Conclusion sections, providing evidence that final_intervention cuases an effect_direction in outcome.
 
+        Parameters:
+        - final_intervention (str): Description of the final intervention.
+        - population (str): Description of the population.
+        - effect_direction (str): Direction of the effect.
+        - outcome (str): Description of the outcome.
+
+        Keyword args:
+        - sample_size (float, optional): The sample size. Default is 2000.
+        - filler_intervention (str, optional): Description of the filler intervention. Default is None, in which case LLM will determine filler_intervention. If LLM fails to do so, "an intervention" is used.
+        - methodology (str, optional): Description of the methodology used. Default is None, in which case LLM will determine methodology. If LLM fails to do so, "randomized controlled trial" is used.
+        - intervention_metric (str): how intervention is measured. Default is None, in which case LLM will determine metric.
+        - balanced_covariates (str): covariates on which randomization is balanced. Default is None, in which case LLM will determine covariates.
+        - outcome_metric (str): how outcome is measured. Default is None, in which case LLM will determine the metric.
+        - editor (bool, optional): Whether an editing LLM should review output. Default is True.
+        - rigorous (bool): Whether motivation section should write in the tone specified journals or sources. Default is True.
+        - journal_list (str, optional): Comma-separated list of target journals/sources in whose tone the paper should be written if rigorous = True. Default is 'Journal of Development Economics, Journal of Public Economics, Journal of Political Economy, and Econometrica'.
+
+
+        Returns:
+        dict: A dictionary containing the outputs of a paper, including code to create data, Motivation section, Methodology section, Conclusion section, Title, full text, and formatted full text.
+        """
+        prompt_kwarg_generator = PromptKwargGenerator(verbose = self.verbose, 
+                                                      llm = self.LLM)
+        prompt_kwargs = prompt_kwarg_generator.get_prompt_kwargs(final_intervention = final_intervention, 
+                                            population = population, 
+                                            outcome = outcome, 
+                                            effect_direction = effect_direction, 
+                                            **kwargs)
+        prompt_dict = PromptDictGenerator().generate_prompt_dict(**prompt_kwargs)
+        output_dict = self.run_prompt(prompt_dict = prompt_dict, 
+                                      editor = prompt_kwargs['editor'],
+                                      rigorous = prompt_kwargs['rigorous'])
+        return output_dict
+
+    def build_section(self, 
+                prompt: str, 
+                editor: bool) -> str:
+        """
+        Writes and edits a section of the research paper.
+
+        Parameters:
+        - prompt (str): prompt to write section
+        - editor (bool): whether second LLM should edit first draft output
+
+        Returns:
+        - str: section text
+        """
+        first_draft = self.LLM.get_response(prompt = prompt)
+        if editor and first_draft!=pconfigs.refusal_response:
+            edited = self.edit_paper(prompt = first_draft) 
+            output = edited if edited !=pconfigs.refusal_response else first_draft
+        else:
+            output = first_draft
+        return output
+
+    def run_prompt(self, 
+                    prompt_dict: dict, 
+                    editor: bool, 
+                    rigorous: bool):
+        """
+        Runs prompts and returns dict with output text
+
+        Parameters:
+        - prompt_dict (dict): dict containing prompts to write paper
+        - editor (bool): whether editor should revise output
+        - rigorous (bool): whether motivation section is written in tone of journals
+
+        Returns:
+        - dict: output with LLM text response to prompts
+        """
+        section_dict = {}
+        for section, prompt in prompt_dict.items():
+            if section == "Motivation_rigorous" and not rigorous:
+                continue
+            section_dict[section] = self.build_section(
+                                        prompt = prompt, 
+                                        editor = editor)
+        output_dict = self._join_sections(section_dict,
+                                          rigorous = rigorous)
+        return output_dict
     
-    def __call__(self, message):
-        self.messages.append({"role": "user", "content": message})
-        result = self.execute()
-        self.messages.append({"role": "assistant", "content": result})
-        return result
-    
-    def execute(self):
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.messages)
-        # Uncomment this to print out token usage each time, e.g.
-        # {"completion_tokens": 86, "prompt_tokens": 26, "total_tokens": 112}
-        # print(completion.usage)
-        return completion.choices[0].message.content
+    def edit_paper(self, 
+                   prompt:str):
+        """
+        Runs first_draft output by editor "Sia" to increase quality.
 
+        Parameters:
+        - prompt (str): input text
 
-class CommaSeparatedListOutputParser(BaseOutputParser):
-    """Parse the output of an LLM call to a comma-separated list."""
+        Returns:
+        - str: input string edited by Sia
+        """
+        output = self.LLM.get_response(prompt, 
+                                   system = prompts.set_paper_editor_sys)
+        return output
 
+    def format_paper(self, 
+                     paper_text: str):
+        """
+        Takes in string text and returns text formatted for LaTeX
 
-    def parse(self, text: str):
-        """Parse the output of an LLM call."""
-        return text.strip().split(", ")
-    
+        Parameters:
+        - prompt (str): input text
 
+        Returns:
+        - str: input text formatted for LaTeX
+        """
+        prompt = prompts.format_paper.format(paper_text=paper_text)
+        output = self.LLM.get_response(prompt = prompt)
+        return output
 
+    def _join_sections(self, 
+                    section_dict:dict,
+                    rigorous: bool) -> dict:
+        """
+        Concatenates sections from section_dict as specified in pconfigs.paper_order. Formats paper text for LaTeX.
 
-for prompt in prompts:
-    # Set up the chain for processing
-    chain = SinglePromptChain(llm=llm, prompt=prompt)
-    
-    # Process the prompt and get the output
-    output = chain.run()
-    
-    # Save the output
-    outputs.append(output)
+        Parameters: 
+        - section_dict (dict): dict whose keys are section name and values are section text
+        - rigorous (bool): whether to write rigorous motivation section using inspiration from literature
 
-# Now you can refer to the outputs later in the code
-for i, output in enumerate(outputs):
-    print(f"Output {i}: {output}")
+        Returns: 
+        - dict: input dictionary with two additional entries, including paper_text (concatenated sections) and formatted_paper_text (concatenated sections in LaTeX formatting)
+        """
+        if rigorous and section_dict['Motivation_rigorous'] != pconfigs.refusal_response:
+            section_dict['Motivation'] = section_dict['Motivation_rigorous']
 
-# Example of referring to a specific output
-# Let's say you want to refer to the second output
-second_output = outputs[1]
-print(f"The second output was: {second_output}")
+        paper_text = ' '.join(f'{s}: {section_dict[s]}' for s in pconfigs.paper_order if section_dict[s]!=pconfigs.refusal_response)
+        paper_text = paper_text.replace('\n', ' ').replace("\'","'" )
+        section_dict['paper_text'] = paper_text
+        section_dict['formatted_paper_text'] = self.format_paper(paper_text)
+
+        return section_dict
